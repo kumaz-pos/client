ca3331f7ff75bc917dc743e57c903ee102f9cea8
kumaz-pos <160744617+kumaz-pos@users.noreply.github.com>
2024-10-07 17:21:33 -0700

    Update AppLayout.jsx

--- client/src/components/common/AppLayout.jsx
+++ client/src/components/common/AppLayout.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './Navbar'
 import {Outlet} from "react-router-dom";
 function AppLayout({data}) {
+  document.title="Japan Direct Point Of Sale"
   return (
     <>
     <Navbar data={data}/>
